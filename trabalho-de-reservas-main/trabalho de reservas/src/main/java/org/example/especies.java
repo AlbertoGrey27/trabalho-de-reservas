877a3643e7c6d24dfa87baf222054c0485b2b66a
AlbertoGrey27 <163658836+AlbertoGrey27@users.noreply.github.com>
2024-10-10 15:54:43 -0300

    Add files via upload

---
+++ trabalho-de-reservas-main/trabalho de reservas/src/main/java/org/example/especies.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class especies {
+    private String nome;
+    private String tipo;
+    private boolean ameacada;
+    private int id;
+
+
+    public especies(String nome, String tipo, boolean ameacada , int id) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.ameacada = ameacada;
+        this.id = id;
+    }
+    public especies() { }
+
+    public void mostrarInfoEspecie() {
+        System.out.printf("{ Nome: %s  Tipo: %s Ameaçada: %b }", nome, tipo, ameacada);
+    }
+
+    public String getNome() {return nome;}
+
+    public void setNome(String nome) {this.nome = nome;}
+
+    public String getTipo() {return tipo;}
+
+    public void setTipo(String tipo) {this.tipo = tipo;}
+
+    public boolean getAmeacada() {return ameacada;}
+
+    public void setAmeacada(boolean ameacada) {this.ameacada = ameacada;}
+
+    public int getId() {return id;}
+
+    public void setId(int id) {this.id = id;}
+}
